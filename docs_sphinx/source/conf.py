7dfe23169c65449a1a79e9531b34a2e6a5f195ea
igomezv <neorelativista@gmail.com>
2026-06-08 17:01:59 +0200

    Docs setup

--- docs_sphinx/source/conf.py
+++ docs_sphinx/source/conf.py
@@ -1,27 +1,29 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = "doppleriann"
-copyright = "2026, Isidro Gómez-Vargas"
-author = "Isidro Gómez-Vargas"
-release = "0.5.0"
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 from pathlib import Path
 import sys
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 
+project = "doppleriann"
+copyright = "2026, Isidro Gómez-Vargas"
+author = "Isidro Gómez-Vargas"
+release = "1.0.0"
+
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
 ]
+
+napoleon_numpy_docstring = True
+napoleon_google_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
 
 autodoc_default_options = {
     "members": True,
@@ -33,13 +35,5 @@ autodoc_member_order = "bysource"
 templates_path = ["_templates"]
 exclude_patterns = []
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    "collapse_navigation": False,
-    "navigation_depth": 4,
-}
 html_static_path = ["_static"]
